9959284820dbf9567f4b1fa83149500390832fdc
Clementb <clement.benoit5@hotmail.com>
2017-03-24 10:53:58 +0100

    ajout parti 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-// Generated on 2017-03-22 using generator-angular 0.16.0
+// Generated on 2017-03-24 using generator-angular 0.16.0
 'use strict';
 
 // # Globbing
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration
-// Generated on 2017-03-22
+// Generated on 2017-03-24
 
 module.exports = function(config) {
   'use strict';
